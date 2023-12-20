6b2e5988c7fdd69c9559b0e1318f9b8d345173c2
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-12-20 09:59:41 +0200

    Pre-builded roles added

--- SIB.Server/SIB.Server/Data/ApplicationDbContext.cs
+++ SIB.Server/SIB.Server/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,40 @@ namespace SIB.Server.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            this.SeedRoles(builder);
+        }
 
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                    new IdentityRole() { Name = "Owner", ConcurrencyStamp = "1", NormalizedName = "Owner".ToUpper()},
+                    new IdentityRole() { Name = "Admin", ConcurrencyStamp = "2", NormalizedName = "Admin".ToUpper() },
+                    new IdentityRole() { Name = "Moderator", ConcurrencyStamp = "3", NormalizedName = "Moderator".ToUpper()},
+                    new IdentityRole() { Name = "Creator", ConcurrencyStamp = "4", NormalizedName = "Creator".ToUpper() }
+                );
+        }
+
+        //public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    var hasher = new PasswordHasher<ApplicationUser>();
+
+        //    builder.Entity<ApplicationUser>().HasData(
+        //            new ApplicationUser
+        //            {
+        //                Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+        //                UserName = "Owner",
+        //                NormalizedUserName = "OWNER".ToUpper(),
+        //                Email = "Owner@owner.com",
+        //                NormalizedEmail = "OWNER@OWNER.COM".ToUpper(),
+        //                EmailConfirmed = true,
+        //                SecurityStamp = new DateTime().ToString(),
+        //                PasswordHash = hasher.HashPassword(null,"OURPASSWORD")
+        //            }
+        //        );
+        //}
     }
 }
---
+++ SIB.Server/SIB.Server/Data/Migrations/20231220075920_Create-Pre-build-Roles.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SIB.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePrebuildRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "418471dc-88a8-4349-ad96-9e7686e8e8a8", "1", "Owner", "OWNER" },
+                    { "5c1b1e9c-58dd-45ba-8229-776fb34feb05", "4", "Creator", "CREATOR" },
+                    { "a2c5106a-f3ac-4492-9019-0b0195885074", "3", "Moderator", "MODERATOR" },
+                    { "daa6d472-4d71-4f32-9558-62e271aaa1a6", "2", "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "418471dc-88a8-4349-ad96-9e7686e8e8a8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5c1b1e9c-58dd-45ba-8229-776fb34feb05");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a2c5106a-f3ac-4492-9019-0b0195885074");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "daa6d472-4d71-4f32-9558-62e271aaa1a6");
+        }
+    }
+}
